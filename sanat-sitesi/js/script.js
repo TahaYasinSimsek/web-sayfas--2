832e95804339049a4e8bdd01d1a543178a1ac767
TahaYasinSimsek <102365056+TahaYasinSimsek@users.noreply.github.com>
2022-04-09 13:21:35 +0300

    web çalışmam

---
+++ sanat-sitesi/js/script.js
@@ -0,0 +1,14 @@
+$(function()
+{
+    let start = 0;
+    let end = $("num").html();
+    let speed = 100;
+
+    setInterval(function(){
+        if(start<end)
+        {
+            start++;
+        }
+        $(".num").html(start);
+    },speed);
+});
